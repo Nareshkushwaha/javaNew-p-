b9ef5a953ba8dd997e9cf22ffa2d53bb8f83d0e1
naresh <nareshkushwaha8003@gmail.com>
2024-09-03 14:48:22 +0530

    this is deep copy frome user input

---
+++ Deepcopy2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Deepcopy2{
+    static void display(int a[]){
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+        System.out.println("this is array second");
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size of array");
+        int size=sc.nextInt();
+        int a[]=new int[size];
+        for(int i=0;i<a.length;i++){
+            a[i]=sc.nextInt();
+        }
+    
+        int b[]=a.clone();
+        display(a);
+        display(b);
+        a[2]=45;
+        b[3]=34;
+        display(a);
+        display(b);
+        
+        
+      
+    }
+}
