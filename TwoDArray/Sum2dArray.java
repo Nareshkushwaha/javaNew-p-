001300de9d7ff96827b8e398937c127a597eedd1
naresh <nareshkushwaha8003@gmail.com>
2024-09-25 11:53:44 +0530

    sum2dArray

---
+++ TwoDArray/Sum2dArray.java
@@ -0,0 +1,14 @@
+public class Sum2dArray {
+    
+    public  static void main(String args[]){
+         int a[][]={{1,2,3},{4,5,6},{7,8,9}};
+         int sum=0;
+          for(int i=0;i<a.length;i++){
+             for(int j=0;j<a[i].length;j++){
+                sum+= a[i][j];
+             }
+          }
+          System.out.println("Toatal sum of array "+sum);
+
+    }
+}
