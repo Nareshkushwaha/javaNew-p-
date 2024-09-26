5989e16d0134bc5fb5b783c72bd8854a562e074f
naresh <nareshkushwaha8003@gmail.com>
2024-09-26 11:07:36 +0530

    Main file

--- Main.java
+++ Main.java
@@ -1,13 +1,40 @@
-public class Main{
-    public static void main(String args[]){
-        int a[]={1,2,3,4,5,6,7,8};
-        int count=0;
-        int target=5;
-        for(int i=0;i<a.length;i++){
-            if(target<a[i]){
-                count++;
-            }
-        }
-        System.out.println(count);
+
+
+
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+
+    void sound(String loudness) {
+        System.out.println("Animal makes a " + loudness + " sound");
     }
 }
+
+class Dog extends Animal {
+    void sound() {
+        System.out.println("Dog barks");
+    }
+
+    void sound(String loudness, String frequency) {
+        System.out.println("Dog barks " + loudness + " and " + frequency);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.sound(); 
+        animal.sound("loud"); 
+
+        Dog dog = new Dog();
+        dog.sound(); 
+        dog.sound("loud", "high"); 
+
+        Animal animalDog = new Dog();
+        animalDog.sound(); 
+        animalDog.sound("loud"); 
+    }
+}
+
+
