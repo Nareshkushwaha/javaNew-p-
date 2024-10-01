4625b16e2215d4a1b7530da32c9b8ac015ae8549
Aryan Nandanwar <116783328+AryanNandanwar@users.noreply.github.com>
2024-10-01 13:13:12 +0530

    Improved readability and optimized maximum calculation

--- MaxArray.java
+++ MaxArray.java
@@ -5,11 +5,9 @@ public  class MaxArray{
           int max=0;
            for(int i=0; i<a.length; i++)
            {
-             if (a[i] > max) 
-                max = a[i];
+             max = Math.max(max, a[i]);
             }
-
-                   System.out.print(max);
-               }
-           }
+      System.out.print(max);
+     }
+}
      
