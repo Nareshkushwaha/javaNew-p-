e5216e843c7a65be4b9f4f131480869588c06d30
naresh <nareshkushwaha8003@gmail.com>
2024-10-01 12:48:10 +0530

    Sechdular thread

---
+++ Multithreading/ThreadSchedular.java
@@ -0,0 +1,20 @@
+package Multithreading;
+
+class MyThread extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("child thread");
+        }
+    }
+}
+
+public class ThreadSchedular {
+    public static void main(String[] args) {
+        // object thread class
+        MyThread t1 = new MyThread();
+        t1.start();
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("parent thread");
+        }
+    }
+}
