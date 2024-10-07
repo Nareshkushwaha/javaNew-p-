624cbe273930f8e520414f92f6615618508aab33
naresh <nareshkushwaha8003@gmail.com>
2024-10-07 10:54:10 +0530

    Finally Catch Arithmetic

---
+++ ExceptionHandling/FinallyCatchArithemetic.java
@@ -0,0 +1,21 @@
+
+// try --->catch-->finally 
+
+package ExceptionHandling;
+
+
+public class FinallyCatchArithemetic {
+        public static void main(String[] args) {
+            try {
+                int a = 10;
+                int b = 0;
+                System.out.println(a / b);
+            } catch (ArithmeticException ex) {
+                System.out.println("Exception Handle");
+            } finally {
+                System.out.println("always executed");
+            }
+        }
+    }
+     
+
