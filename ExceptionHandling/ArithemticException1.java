101b80d50fc54205d398e58dadff2d8256196ae8
naresh <nareshkushwaha8003@gmail.com>
2024-10-07 10:44:33 +0530

    ArtemeticException

---
+++ ExceptionHandling/ArithemticException1.java
@@ -0,0 +1,10 @@
+ package ExceptionHandling;
+
+public class ArithemticException1{
+    public static void main(String[] args) {
+        int a=7;
+        int b=0;
+        int c=a/b;
+        System.out.println(c);
+    }
+}
