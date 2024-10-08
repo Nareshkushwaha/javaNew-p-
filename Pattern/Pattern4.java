ce3a082fbd16118893b41999cfc88d9d250c4eba
naresh <nareshkushwaha8003@gmail.com>
2024-10-08 14:25:54 +0530

    Patttern4

---
+++ Pattern/Pattern4.java
@@ -0,0 +1,15 @@
+package Pattern;
+
+public class Pattern4{
+    public static void main(String args[]){
+        for(int i=0;i<=5;i++){
+             for(int j=i;j<5;j++){
+                 System.out.print(" ");
+             }
+             for(int k=1;k<=i;k++){
+                 System.out.print("*");
+             }
+             System.out.println();
+        }
+    }
+}
