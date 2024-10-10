9d0fc461cd092d3ad8ea2190725b203e230267dc
naresh <nareshkushwaha8003@gmail.com>
2024-10-10 14:37:55 +0530

    Throwcode2

---
+++ ExceptionHandling/ThrowCode2.java
@@ -0,0 +1,25 @@
+package ExceptionHandling;
+
+class A extends RuntimeException {
+    A(String name) {
+        System.out.println(name);
+    }
+}
+
+class B {
+    void hello() {
+        int a = 18;
+        if (a > 10) {
+            throw new A("a is greater than");
+        } else {
+            System.out.println("a is less than 10 ");
+        }
+    }
+}
+
+public class ThrowCode2 {
+    public static void main(String[] args) {
+        B p = new B();
+        p.hello();
+    }
+}
