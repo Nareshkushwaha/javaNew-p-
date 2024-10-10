62a326aa20cf95714d3b1c062d1ca70b0c435d0a
naresh <nareshkushwaha8003@gmail.com>
2024-10-10 14:21:34 +0530

    ThrowCase5

---
+++ ExceptionHandling/ThrowCase5.java
@@ -0,0 +1,9 @@
+package ExceptionHandling;
+
+public class ThrowCase5 {
+    public static void main(String[] args) {
+        //class used as exception error 
+        throw new ThrowCase5();
+        //it give compile time errror 
+    }
+}
