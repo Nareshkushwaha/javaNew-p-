9db52f0df031ce138c3f2d7677fc1e586f031ce7
naresh <nareshkushwaha8003@gmail.com>
2024-10-11 17:51:11 +0530

    ThrowsCase1

---
+++ ExceptionHandling/ThrowsCase1.java
@@ -0,0 +1,11 @@
+package ExceptionHandling;
+
+public class ThrowsCase1{
+    static void show() throws ArithmeticException {
+         System.out.println("Handle");
+    }
+    
+     public static void main(String args[]){
+          show();
+     }
+}
