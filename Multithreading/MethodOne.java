1e8bb78a897281e6dab5dfe1797c933777b062e6
naresh <nareshkushwaha8003@gmail.com>
2024-10-16 20:26:24 +0530

    Yield method

---
+++ Multithreading/MethodOne.java
@@ -0,0 +1,23 @@
+package Multithreading;
+
+class A extends Thread {
+    public void run() {
+        for (int i = 1; i <= 3; i++) {
+
+            System.out.println("parent child");
+        }
+    }
+}
+
+public class MethodOne{
+    public static void main(String[] args) {
+        A k = new A();
+        k.start();
+        A p = new A();
+        p.start();
+        for (int i = 1; i <= 3; i++) {
+            Thread.yield();
+            System.out.println("main thread");
+        }
+    }
+}
