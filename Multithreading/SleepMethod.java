bef48d7fc5aedbc5de5fc782d92e36ccdcb690ad
naresh <nareshkushwaha8003@gmail.com>
2024-10-16 20:28:30 +0530

    Sleep method

---
+++ Multithreading/SleepMethod.java
@@ -0,0 +1,23 @@
+package Multithreading;
+
+class A extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException ex) {
+
+            }
+            System.out.print(i + " ");
+        }
+    }
+}
+
+public class SleepMethod{
+    public static void main(String[] args) {
+        A p = new A();
+        A k = new A();
+        p.start();
+        k.start();
+    }
+}
