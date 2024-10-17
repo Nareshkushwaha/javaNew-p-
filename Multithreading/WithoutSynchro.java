97555853c80c6e0d1320bcc1047250e0b05de5a7
naresh <nareshkushwaha8003@gmail.com>
2024-10-17 12:21:16 +0530

    WithotStnchro

---
+++ Multithreading/WithoutSynchro.java
@@ -0,0 +1,28 @@
+package Multithreading;
+class A {
+    String name;
+
+    A(String name) {
+        this.name = name;
+    }
+}
+
+class B {
+    A p;// class refrence variable
+
+    B(A p) {
+        this.p = p;
+    }
+
+    void show() {
+        System.out.println(p.name);
+    }
+}
+
+public class WithoutSynchro {
+    public static void main(String[] args) {
+        A k = new A("kajal");
+        B l = new B(k);
+        l.show();
+    }
+}
