febba6e593a5617c7eec3671bade2b7890fdf658
naresh <nareshkushwaha8003@gmail.com>
2024-10-26 09:31:51 +0530

    wrapper class convert string

---
+++ StringWrapper/WrapperClassConvertString.java
@@ -0,0 +1,8 @@
+package StringWrapper;
+
+public class WrapperClassConvertString {
+    public static void main(String[] args) {
+        Integer i=new Integer(87);
+        System.out.println(i.toString(i));
+    }
+}
