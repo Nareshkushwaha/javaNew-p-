3c99f9e64da7ea272323afcd17d64fc51339ddeb
naresh <nareshkushwaha8003@gmail.com>
2024-10-26 09:37:58 +0530

    Wc convertPD

---
+++ StringWrapper/WcConvertPD.java
@@ -0,0 +1,10 @@
+package StringWrapper;
+
+public class WcConvertPD {
+    public static void main(String[] args) {
+        // autoboxing : value
+        Integer i = 78;
+        int j = i;
+        System.out.println(j);
+    }
+}
