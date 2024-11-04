0bce1a2b141be8c49bb43a75c1d048e2e7e47c71
naresh <nareshkushwaha8003@gmail.com>
2024-11-04 15:52:31 +0530

    how to store string data in the memory

---
+++ String/LengthOfString2.java
@@ -0,0 +1,12 @@
+package String;
+
+public class LengthOfString2 {
+    public static void main(String[] args) {
+        String name = "Regex";
+        System.out.println(name);
+        System.out.println(name.length());
+
+        String name1 = "hello";
+        System.out.println(name1);
+    }
+}
