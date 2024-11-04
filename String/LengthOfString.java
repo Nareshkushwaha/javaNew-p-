bb7e28e9749e7c31455e51d23549e782bef91c54
naresh <nareshkushwaha8003@gmail.com>
2024-11-04 15:49:11 +0530

    Length of String

---
+++ String/LengthOfString.java
@@ -0,0 +1,11 @@
+package String;
+
+public class LengthOfString{
+    public static void main(String[] args) {
+        String name="Regex";
+        System.out.println(name.length());
+
+        int a[]={1,2,3,4,5};
+        System.out.println(a.length);
+    }
+}
