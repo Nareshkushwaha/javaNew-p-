347d66353f39c61dcdf9d95dff761ee4ceb49ccb
naresh <nareshkushwaha8003@gmail.com>
2024-11-04 16:23:53 +0530

    StringCreation

---
+++ String/Creationstring.java
@@ -0,0 +1,13 @@
+package String;
+
+public class Creationstring{
+    public static void main(String[] args) {
+        // string constant| iteral form
+        String name = "kajal";
+        System.out.println(name);
+
+        // string dynamic form
+        String name1 = new String("hello world");
+        System.out.println(name1);
+    }
+}
