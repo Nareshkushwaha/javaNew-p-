62e50ec935469f852c7f5d4ebb4797a52c6766e5
naresh <nareshkushwaha8003@gmail.com>
2024-11-04 15:01:08 +0530

    Basic string

---
+++ String/BasicString.java
@@ -0,0 +1,8 @@
+package String;
+
+public class BasicString {
+    public static void main(String[] args) {
+        String name = "regex is a learning platform";
+        System.out.println(name);
+    }
+}
