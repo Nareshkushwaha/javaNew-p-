275d333ae2040926c9f55a58b90bbc6c93fca9ae
naresh <nareshkushwaha8003@gmail.com>
2024-11-07 11:51:11 +0530

    String method one

---
+++ Substring/Stringmethod1.java
@@ -0,0 +1,11 @@
+package Substring;
+
+public class Stringmethod1{
+    public static void main(String[] args) {
+        String name = "kajal";
+        System.out.println(name.charAt(2));
+        char s = name.charAt(9);
+        System.out.println(s);
+        // j and string index out of bound
+    }
+}
