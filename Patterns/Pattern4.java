0c421c78d21daf580794d269acc5d0616f799b18
naresh <nareshkushwaha8003@gmail.com>
2024-11-11 11:54:58 +0530

    pattern 4

---
+++ Patterns/Pattern4.java
@@ -0,0 +1,33 @@
+package Patterns;
+
+import java.util.Scanner;
+ 
+public class Pattern4 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+ 
+        //Taking rows value from the user
+ 
+        System.out.println("How many rows you want in this pattern?");
+ 
+        int rows = sc.nextInt();
+ 
+        System.out.println("Here is your pattern....!!!");
+ 
+        for (int i = rows; i >= 1; i--)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(i+" ");
+            }
+ 
+            System.out.println();
+        }
+ 
+        //Close the resources
+ 
+        sc.close();
+    }
+}
