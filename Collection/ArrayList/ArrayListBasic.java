021201e8489772414a00608d10d08f32933c8824
naresh <nareshkushwaha8003@gmail.com>
2024-11-13 12:58:56 +0530

    Array List Basic

---
+++ Collection/ArrayList/ArrayListBasic.java
@@ -0,0 +1,15 @@
+package Collection.ArrayList;
+
+import java.util.*;
+
+public class ArrayListBasic {
+    public static void main(String[] args) {
+        ArrayList al = new ArrayList();
+        // add element add method
+        al.add("hello");
+        al.add(45);
+        al.add('u');
+        al.add(23.4);
+        System.out.println(al);
+    }
+}
