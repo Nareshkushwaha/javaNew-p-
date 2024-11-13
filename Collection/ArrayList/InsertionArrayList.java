7a0694f4d721637be42c177d749c209fc80a825d
naresh <nareshkushwaha8003@gmail.com>
2024-11-13 13:02:39 +0530

    InsertionArrayList

---
+++ Collection/ArrayList/InsertionArrayList.java
@@ -0,0 +1,14 @@
+package Collection.ArrayList;
+
+import java.util.*;
+
+public class InsertionArrayList {
+    public static void main(String[] args) {
+        ArrayList al = new ArrayList();
+        al.add("hello");
+        al.add(657);
+        al.add(5.67);
+        System.out.println(al);
+        System.out.println(al.add("hii"));
+    }
+}
