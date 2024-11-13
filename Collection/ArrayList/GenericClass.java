dff858dd6b4ddf93195ebe42dab8d78db213b4a4
naresh <nareshkushwaha8003@gmail.com>
2024-11-13 13:03:39 +0530

    Generic class

---
+++ Collection/ArrayList/GenericClass.java
@@ -0,0 +1,14 @@
+package Collection.ArrayList;
+
+import java.util.*;
+
+public class GenericClass {
+    public static void main(String[] args) {
+        // homogenous data stored
+        ArrayList<String> al = new ArrayList<>();
+        al.add("hello");
+        al.add("bye");
+        al.add("hii");
+        System.out.println(al);
+    }
+}
