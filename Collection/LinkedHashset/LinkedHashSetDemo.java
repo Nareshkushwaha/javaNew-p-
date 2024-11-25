012ea83264285045c46767d8e196bf5b50242bc8
naresh <nareshkushwaha8003@gmail.com>
2024-11-25 11:35:22 +0530

    Linked hasSet Demo

---
+++ Collection/LinkedHashset/LinkedHashSetDemo.java
@@ -0,0 +1,12 @@
+package Collection.LinkedHashset;
+
+import java.util.*;
+
+public class LinkedHashSetDemo {
+    public static void main(String[] args) {
+        LinkedHashSet<String> lhs = new LinkedHashSet<>();
+        lhs.add("Apple");
+        lhs.add("hello");
+        System.out.println(lhs);
+    }
+}
