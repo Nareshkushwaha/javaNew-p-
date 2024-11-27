bfcb597d395351c15e3dc88235ab7662d9d32b4b
naresh <nareshkushwaha8003@gmail.com>
2024-11-27 19:45:38 +0530

    Tree Set Creation

---
+++ Collection/TreeSet/TreeSetCreation.java
@@ -0,0 +1,14 @@
+package Collection.TreeSet;
+
+import java.util.*;
+
+public class TreeSetCreation {
+    public static void main(String[] args) {
+        TreeSet<Integer> ts = new TreeSet<>();
+        ts.add(6);
+        ts.add(1);
+        ts.add(9);
+        ts.add(3);
+        System.out.println(ts);
+    }
+}
