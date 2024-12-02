2a86ebd70a0869b37082bbcf7babcbf799040f38
naresh <nareshkushwaha8003@gmail.com>
2024-12-02 11:37:49 +0530

    Vector Basic

---
+++ Collection/Vactor/VactorBasic.java
@@ -0,0 +1,13 @@
+package Collection.Vactor;
+
+import java.util.Vector;
+
+public class VactorBasic {
+    public static void main(String[] args) {
+        Vector v = new Vector<>();
+        v.addElement(78);
+        v.addElement(23);
+        v.addElement(56);
+        System.out.println(v);
+    }
+}
